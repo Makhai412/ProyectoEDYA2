d1b16df4c1c78016a12512aaf40cfb6ce50f4211
soto30andres <andres_f.soto@uao.edu.co>
2024-04-24 11:59:32 -0500

    soto30andres: added popup to login

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,11 @@ import LogIn from "./pages/login/LogIn";
 import SignUp from "./pages/login/SignUp";
 
 function App() {
+  
   return (
 
     <main className="">
       <NavbarComponent/>
-      <SignUp/>
-      <LogIn/>
     </main>
   );
 }
--- src/components/navbar/NavBarComponent.jsx
+++ src/components/navbar/NavBarComponent.jsx
@@ -1,5 +1,6 @@
-import { Button, Navbar, NavbarBrand, NavbarCollapse, NavbarLink, NavbarToggle } from "flowbite-react";
-
+import { Button, Navbar, NavbarBrand, NavbarCollapse, NavbarLink, NavbarToggle, Checkbox, Label, Modal, TextInput } from "flowbite-react";
+import { useState, useRef } from 'react'
+import LogIn from "../../pages/login/LogIn";
 
 let customButtonTheme = {
   color: {
@@ -10,14 +11,19 @@ let customButtonTheme = {
 
 
  const NavbarComponent = () => {
+
+  const [openModal, setOpenModal] = useState(false);
+  //const emailInputRef = useRef<HTMLInputElement>(null);
+
   return (
+    <>
     <Navbar className="bg-red-700" fluid>
       <NavbarBrand href="https://flowbite-react.com">
         <img src="/logo3.png" className="mr-3 h-6 sm:h-20" alt="Flowbite React Logo" />
         <span className="self-center text-white text-xl font-semibold">Cinema UAO </span>
       </NavbarBrand>
       <div className="flex md:order-2">
-        <Button theme={customButtonTheme} color="primary">Login</Button>
+        <Button theme={customButtonTheme} onClick={() => setOpenModal(true)} color="primary">Login</Button>
         <NavbarToggle />
       </div>
       <NavbarCollapse>
@@ -28,6 +34,14 @@ let customButtonTheme = {
         <NavbarLink href="#" className="text-white">Categoria</NavbarLink>
       </NavbarCollapse>
     </Navbar>
+
+    <Modal show={openModal} size="md" popup onClose={() => setOpenModal(false)}>
+    <Modal.Header />
+    <Modal.Body>
+      <LogIn></LogIn>
+    </Modal.Body>
+    </Modal>
+    </>
   );
 
 }
