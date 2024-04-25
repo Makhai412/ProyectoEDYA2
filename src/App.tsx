107f43a58ebaaa8e2c5aee599838e945880a4ce4
Felipe <afgomez0622@gmail.com>
2024-04-25 11:56:09 -0500

    MenuComponent added

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,16 @@ import { DarkThemeToggle } from "flowbite-react";
 import MyButton from "./components/navbar/NavBarComponent";
 import LogIn from "./pages/login/LogIn";
 import SignUp from "./pages/login/SignUp";
+import { MenuComponent } from "./components/shared/MenuComponent"
 
 function App() {
   return (
 
     <main className="">
-      <MyButton/>
-      <SignUp/>
+      <MyButton />
+      <SignUp />
+      <MenuComponent />
+
     </main>
   );
 }
